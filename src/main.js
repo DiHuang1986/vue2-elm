a9e7cd401a483f2c61604ea01c0d2f37a858932d
Di Huang <huang.di@live.com>
2018-04-13 11:13:34 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import routes from './router/router'
 import store from './store/'
 import {routerMode} from './config/env'
 import './config/rem'
+
+// for better click experience, in normal click event, it will delay 300 ms to response, FastClick will remove it
 import FastClick from 'fastclick'
 
+// when DOM loaded, attach FastClick
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function() {
         FastClick.attach(document.body);
@@ -13,14 +16,21 @@ if ('addEventListener' in document) {
 }
 
 Vue.use(VueRouter)
+
+// TODO need to view router configuration
 const router = new VueRouter({
 	routes,
+    // TODO mode?
 	mode: routerMode,
+    // TODO strict?
 	strict: process.env.NODE_ENV !== 'production',
+    // for scroll behavior
 	scrollBehavior (to, from, savedPosition) {
+        // if has savedPosition, go savedPosition
 	    if (savedPosition) {
 		    return savedPosition
 		} else {
+            
 			if (from.meta.keepAlive) {
 				from.meta.savedPosition = document.body.scrollTop;
 			}
